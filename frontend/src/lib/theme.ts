36c4aa2c5c12c5ed783968a0d4eef774923dc760
manikumarchintapall <157659942+manikumarchintapall@users.noreply.github.com>
2024-03-04 00:07:42 -0500

    protected routes on frontend

--- frontend/src/lib/theme.ts
+++ frontend/src/lib/theme.ts
@@ -3,5 +3,8 @@ import { createTheme } from "@mui/material";
 export const theme = createTheme({
   palette: {
     mode: "light",
+    // secondary: {
+    //   main: "#255",
+    // },
   },
 });
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,8 +1,10 @@
 import SignInPage from "@/pages/Auth/Sign-In";
 import SignUpPage from "@/pages/Auth/Sign-Up";
 import HomePage from "@/pages/Home";
+import { getSignedInUserDetails } from "@/utils/authUtils";
+import { Box, Typography } from "@mui/material";
 import React from "react";
-import { Outlet, useRoutes } from "react-router-dom";
+import { Navigate, Outlet, useRoutes } from "react-router-dom";
 
 const AppRoutes: React.FC = () => {
   const routes = useRoutes([
@@ -11,32 +13,58 @@ const AppRoutes: React.FC = () => {
       element: <HomePage />,
     },
     {
-      path: "/customer",
-      element: <Outlet />,
+      path: "/auth",
+      element: <AuthRoutesWrapper />,
       children: [
         {
-          index: true,
-          element: <SignInPage />,
+          path: "customer",
+          element: <Outlet />,
+          children: [
+            {
+              index: true,
+              element: <SignInPage />,
+            },
+            {
+              path: "sign-up",
+              element: <SignUpPage />,
+            },
+          ],
         },
         {
-          path: "sign-up",
-          element: <SignUpPage />,
+          path: "vendor",
+          element: <Outlet />,
+          children: [
+            {
+              index: true,
+              element: <SignInPage vendor />,
+            },
+            {
+              path: "sign-up",
+              element: <SignUpPage vendor />,
+            },
+          ],
         },
       ],
     },
     {
-      path: "/vendor",
-      element: <Outlet />,
-      children: [
-        {
-          index: true,
-          element: <SignInPage vendor />,
-        },
-        // {
-        //   path: "sign-up",
-        //   element: <SignUpPage vendor />,
-        // },
-      ],
+      path: "*",
+      element: (
+        <Box
+          height="70vh"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Typography
+            variant="h2"
+            textAlign="center"
+            color="white"
+            fontWeight={600}
+          >
+            404 Page Not Found
+          </Typography>
+        </Box>
+      ),
     },
   ]);
 
@@ -44,3 +72,25 @@ const AppRoutes: React.FC = () => {
 };
 
 export default AppRoutes;
+
+/**
+ * Authentication pages HOC
+ */
+const AuthRoutesWrapper: React.FC = () => {
+  const user = getSignedInUserDetails();
+  if (user) {
+    return <Navigate to="/" replace />;
+  }
+  return <Outlet />;
+};
+
+/**
+ * Protected pages HOC
+ */
+const ProtectedPagesWrapper: React.FC = () => {
+  const user = getSignedInUserDetails();
+  if (!user) {
+    return <Navigate to="/auth/customer" replace />;
+  }
+  return <Outlet />;
+};
