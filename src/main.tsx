26e8ddec3268853e0b4b3e3e86f6d200e5f27771
manikumarchintapall <157659942+manikumarchintapall@users.noreply.github.com>
2024-03-03 19:49:31 -0500

    home page design

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,7 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import { ThemeProvider } from "@mui/material";
 import { QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,69 @@
-import { Box, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Paper,
+  Stack,
+  Typography,
+  TypographyProps,
+} from "@mui/material";
 import React from "react";
 
 const HomePage: React.FC = () => {
   return (
-    <Box position="relative">
-      <img
-        src="/home-bg.jpg"
-        style={{ width: "91vw", maxHeight: "83vh", opacity: "0.8" }}
-      />
-      <Typography position="absolute" top={0}>
-        A Glory of flowers
-      </Typography>
+    <Box
+      textAlign="center"
+      component={Paper}
+      bgcolor="transparent"
+      height="calc(100vh - 10rem)"
+      elevation={24}
+    >
+      <Box
+        display="flex"
+        flexDirection="column"
+        gap="2rem"
+        color={({ palette }) => palette.background.default}
+        alignItems="center"
+        mt="-4rem"
+        pt="4rem"
+      >
+        <TitleTypography>Unleash Your Floral Fantasies</TitleTypography>
+        <Typography fontSize="2rem" fontWeight={600}>
+          Have no idea what flowers to give your loved ones? <br /> Explore our
+          blossom store
+        </Typography>
+        <Stack direction="row" gap="1rem">
+          <Button variant="contained" color="secondary">
+            Explore our store
+          </Button>
+          <Button color="inherit" variant="outlined">
+            About Us &rarr;
+          </Button>
+        </Stack>
+      </Box>
     </Box>
   );
 };
 
 export default HomePage;
+
+/**
+ * ============ CUSTOM TYPOGRAPHY ==========
+ */
+
+export const TitleTypography: React.FC<TypographyProps> = (props) => {
+  return (
+    <Typography
+      variant="h3"
+      fontSize="7rem"
+      fontWeight={600}
+      sx={({ palette }) => ({
+        WebkitTextFillColor: palette.background.paper,
+        WebkitTextStroke: "0.5px",
+      })}
+      color={({ palette }) => palette.secondary.main}
+      {...props}
+    >
+      {props.children}
+    </Typography>
+  );
+};
